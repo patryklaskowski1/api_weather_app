4fdeef89ef3582ef17fee5ceaf7ab1264eaf5c64
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-20 19:12:23 +0200

    Decoration TextField in home_page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -7,14 +7,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(70.0),
+          padding: const EdgeInsets.all(70.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              TextField(),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'City',
+                  label: const Text(
+                    'Enter the city ',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Color.fromARGB(255, 184, 181, 181),
+                    ),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 184, 181, 181),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 5, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 5, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
