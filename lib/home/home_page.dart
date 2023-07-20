49ee574ac442d81dd432c94e7ff93cd639f915b1
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-20 19:10:00 +0200

    Create TextField and add padding widget

---
+++ lib/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(70.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextField(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
