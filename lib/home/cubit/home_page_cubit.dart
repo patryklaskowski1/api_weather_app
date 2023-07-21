f5417a6fa0e5f951f9789ab0e68db1330b2b108e
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-21 17:09:24 +0200

    Create Cubit for home page

---
+++ lib/home/cubit/home_page_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+
+
+part 'home_page_state.dart';
+
+class HomePageCubit extends Cubit<HomePageState> {
+  HomePageCubit() : super(HomePageState());
+}
+
---
+++ lib/home/cubit/home_page_state.dart
@@ -0,0 +1,3 @@
+part of 'home_page_cubit.dart';
+
+class HomePageState {}
