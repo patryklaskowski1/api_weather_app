4f8113b24f92a6d31c1bbc23c9951a856f9738a6
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-21 17:50:45 +0200

    Add BlockBuilder adn BlockProvider to home_page

--- lib/home/cubit/home_page_cubit.dart
+++ lib/home/cubit/home_page_cubit.dart
@@ -1,11 +1,34 @@
 import 'package:api_weather_app/core/enum.dart';
 import 'package:api_weather_app/domain/models/weather_model.dart';
+import 'package:api_weather_app/domain/repositories/repositories_weather.dart';
 import 'package:bloc/bloc.dart';
-
 
 part 'home_page_state.dart';
 
 class HomePageCubit extends Cubit<HomePageState> {
-  HomePageCubit() : super(HomePageState());
-}
+  HomePageCubit(this._weatherRepository) : super(const HomePageState());
 
+  final WeatherRepository _weatherRepository;
+
+  Future<void> getWetherModel({
+    required String city,
+  }) async {
+    emit(const HomePageState(status: Status.loading));
+    try {
+      final weatherModel = await _weatherRepository.getWeatherModel(city: city);
+      emit(
+        HomePageState(
+          model: weatherModel,
+          status: Status.success,
+        ),
+      );
+    } catch (error) {
+      emit(
+        HomePageState(
+          status: Status.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:api_weather_app/domain/repositories/repositories_weather.dart';
 import 'package:api_weather_app/home/aspect/text_field_search_city.dart';
+import 'package:api_weather_app/home/cubit/home_page_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -8,38 +11,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Padding(
-        padding: EdgeInsets.all(70.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TextFieldSearchCity(),
-            SizedBox(
-              height: 12,
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Check'),
-            ),
-            SizedBox(height: 22),
-            Text(
-              'City',
-              style: TextStyle(
-                fontSize: 24,
+    return BlocProvider(
+      create: (context) => HomePageCubit(WeatherRepository()),
+      child: BlocBuilder<HomePageCubit, HomePageState>(
+        builder: (context, state) {
+          return const Scaffold(
+            body: Center(
+                child: Padding(
+              padding: EdgeInsets.all(70.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextFieldSearchCity(),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(
+                    onPressed: null,
+                    child: Text('Check'),
+                  ),
+                  SizedBox(height: 22),
+                  Text(
+                    'City',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    '18℃',
+                    style: TextStyle(
+                      fontSize: 72,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 12),
-            Text(
-              '18℃',
-              style: TextStyle(
-                fontSize: 72,
-              ),
-            ),
-          ],
-        ),
-      )),
+            )),
+          );
+        },
+      ),
     );
   }
 }
