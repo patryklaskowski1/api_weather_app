84bd117a68256e7f6fa164fe36947c4cb428e3eb
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-22 16:34:09 +0200

    Extract SearchWidget to a seperate folder

---
+++ lib/home/aspect/search_widget.dart
@@ -0,0 +1,37 @@
+import 'package:api_weather_app/home/aspect/text_field_search_city.dart';
+import 'package:api_weather_app/home/cubit/home_page_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchWidget extends StatelessWidget {
+  SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: TextFieldSearchCity(),
+          ),
+          const SizedBox(width: 50),
+          ElevatedButton(
+            onPressed: () {
+              context
+                  .read<HomePageCubit>()
+                  .getWeatherModel(city: _controller.text);
+            },
+            child: const Text('Check'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:api_weather_app/core/enum.dart';
 import 'package:api_weather_app/domain/models/weather_model.dart';
 import 'package:api_weather_app/domain/repositories/repositories_weather.dart';
 import 'package:api_weather_app/home/aspect/display_weather_widget.dart';
+import 'package:api_weather_app/home/aspect/search_widget.dart';
 import 'package:api_weather_app/home/aspect/text_field_search_city.dart';
 import 'package:api_weather_app/home/cubit/home_page_cubit.dart';
 import 'package:flutter/material.dart';
@@ -42,7 +43,7 @@ class HomePage extends StatelessWidget {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      _SearchWidget(),
+                      SearchWidget(),
                       if (weatherModel != null)
                         DisplayWeatherWidget(
                           weatherModel: weatherModel,
@@ -54,40 +55,6 @@ class HomePage extends StatelessWidget {
             );
           },
         ),
-      ),
-    );
-  }
-}
-
-
-class _SearchWidget extends StatelessWidget {
-  _SearchWidget({
-    Key? key,
-  }) : super(key: key);
-
-  final _controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
-            child: TextFieldSearchCity(),
-          ),
-          const SizedBox(width: 50),
-          ElevatedButton(
-            onPressed: () {
-              context
-                  .read<HomePageCubit>()
-                  .getWeatherModel(city: _controller.text);
-            },
-            child: const Text('Check'),
-          ),
-        ],
       ),
     );
   }
