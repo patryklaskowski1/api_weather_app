d584c8e3c212dc40bcb7b84e10184253edc13e2d
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-22 16:32:27 +0200

    Extract displayWeatherWidget to a seperate folder

---
+++ lib/home/aspect/display_weather_widget.dart
@@ -0,0 +1,43 @@
+
+import 'package:api_weather_app/domain/models/weather_model.dart';
+import 'package:api_weather_app/home/cubit/home_page_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DisplayWeatherWidget extends StatelessWidget {
+  const DisplayWeatherWidget({
+    Key? key,
+    required this.weatherModel,
+  }) : super(key: key);
+
+  final WeatherModel weatherModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomePageCubit, HomePageState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                weatherModel.city,
+                style: const TextStyle(
+                  fontSize: 26,
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                '${weatherModel.temperature.toString()} ℃',
+                style: const TextStyle(
+                  fontSize: 76,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:api_weather_app/core/enum.dart';
 import 'package:api_weather_app/domain/models/weather_model.dart';
 import 'package:api_weather_app/domain/repositories/repositories_weather.dart';
+import 'package:api_weather_app/home/aspect/display_weather_widget.dart';
 import 'package:api_weather_app/home/aspect/text_field_search_city.dart';
 import 'package:api_weather_app/home/cubit/home_page_cubit.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,7 @@ class HomePage extends StatelessWidget {
       child: BlocListener<HomePageCubit, HomePageState>(
         listener: (context, state) {
           if (state.status == Status.error) {
-            final errorMessage = state.errorMessage ?? 'Unkown error';
+            final errorMessage = state.errorMessage ?? 'Unkown error ! :(';
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text(errorMessage),
@@ -43,7 +44,7 @@ class HomePage extends StatelessWidget {
                     children: [
                       _SearchWidget(),
                       if (weatherModel != null)
-                        _DisplayWeatherWidget(
+                        DisplayWeatherWidget(
                           weatherModel: weatherModel,
                         ),
                     ],
@@ -58,43 +59,6 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class _DisplayWeatherWidget extends StatelessWidget {
-  const _DisplayWeatherWidget({
-    Key? key,
-    required this.weatherModel,
-  }) : super(key: key);
-
-  final WeatherModel weatherModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<HomePageCubit, HomePageState>(
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                weatherModel.city,
-                style: const TextStyle(
-                  fontSize: 26,
-                ),
-              ),
-              const SizedBox(height: 12),
-              Text(
-                '${weatherModel.temperature.toString()} ℃',
-                style: const TextStyle(
-                  fontSize: 76,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
 
 class _SearchWidget extends StatelessWidget {
   _SearchWidget({
