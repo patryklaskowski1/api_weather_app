aafb722aa12d08e2a19557ac4bea99899e106519
Patryk Laskowski <patrick_laskowski@hotmail.com>
2023-07-23 17:42:13 +0200

    add link with api key

--- lib/domain/repositories/repositories_weather.dart
+++ lib/domain/repositories/repositories_weather.dart
@@ -1,11 +1,21 @@
 import 'package:api_weather_app/domain/models/weather_model.dart';
+import 'package:dio/dio.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    //http://api.weatherapi.com/v1/current.json?key=d5d703a09d3e471cbb7145803232307&q=Kwidzyn&aqi=no
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=d5d703a09d3e471cbb7145803232307&q=$city&aqi=no');
+    final responseData = response.data;
 
-    return const WeatherModel(city: 'Kwidzyn', temperature: 20.0);
+    if (responseData == null) {
+      return null;
+    }
+
+    final cityName = responseData['location']['name'] as String;
+    final temperatureC = (responseData['current']['temp_c'] + 0.0) as double;
+
+    return WeatherModel(city: cityName, temperature: temperatureC);
   }
 }
